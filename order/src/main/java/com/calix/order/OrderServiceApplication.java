5a8657bfe2821c626def807d66234a3e03b39aa2
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-09-05 16:21:57 -0600

    order service initial setup

---
+++ order/src/main/java/com/calix/order/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.calix.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
---
+++ order/src/test/java/com/calix/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calix.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
