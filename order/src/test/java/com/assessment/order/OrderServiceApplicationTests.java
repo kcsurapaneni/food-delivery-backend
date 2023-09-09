d470dbea63fc68ba9fcd5aade363fc1f4a99ebf5
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-09-09 09:49:31 -0600

    test assertions are created

--- order/src/test/java/com/assessment/order/OrderServiceApplicationTests.java
+++ order/src/test/java/com/assessment/order/OrderServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package com.assessment.order;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.assessment.order.repository.*;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.test.context.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private ItemsRepository itemsRepository;
+
+    @Test
+    void contextLoads() {
+        assertThat(customerRepository).isNotNull();
+        assertThat(itemsRepository).isNotNull();
+
+        assertThat(customerRepository.findAll()).isNotEmpty().hasSize(3);
+        assertThat(itemsRepository.findAll()).isNotEmpty().hasSize(5);
+    }
 
 }
--- order/src/test/java/com/assessment/order/TestOrderServiceApplication.java
+++ order/src/test/java/com/assessment/order/TestOrderServiceApplication.java
@@ -1,30 +1,29 @@
 package com.assessment.order;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.springframework.boot.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.boot.testcontainers.service.connection.*;
+import org.springframework.context.annotation.*;
+import org.testcontainers.containers.*;
+import org.testcontainers.utility.*;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestOrderServiceApplication {
 
-	@Bean
-	@ServiceConnection
-	KafkaContainer kafkaContainer() {
-		return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.0"));
+    }
 
-	@Bean
-	@ServiceConnection
-	MySQLContainer<?> mysqlContainer() {
-		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>(DockerImageName.parse("mysql:8.0.23"));
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.from(OrderServiceApplication::main).with(TestOrderServiceApplication.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(OrderServiceApplication::main).with(TestOrderServiceApplication.class).run(args);
+    }
 
 }
