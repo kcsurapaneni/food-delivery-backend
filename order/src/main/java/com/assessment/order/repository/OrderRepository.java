e4071f99c54b3f12cf651442a31076c711ceeac2
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-09-10 13:31:13 -0600

    order update status query is added

--- order/src/main/java/com/assessment/order/repository/OrderRepository.java
+++ order/src/main/java/com/assessment/order/repository/OrderRepository.java
@@ -1,11 +1,17 @@
 package com.assessment.order.repository;
 
+import com.assessment.order.dto.*;
 import com.assessment.order.model.*;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.*;
 
 /**
  * @author Krishna Chaitanya
  */
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    @Modifying
+    @Query("UPDATE Order o SET o.orderStatus = :orderStatus WHERE o.id = :id")
+    int updateOrderStatusById(@Param("id") Integer id, @Param("orderStatus") OrderStatus orderStatus);
 
 }
