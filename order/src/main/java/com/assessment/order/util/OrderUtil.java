af86351d0a4bf6775a4faa53eeabeba64e1b9d28
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-09-11 00:31:46 -0600

    refactored, moved some of the reusable functions to util package

---
+++ order/src/main/java/com/assessment/order/util/OrderUtil.java
@@ -0,0 +1,25 @@
+package com.assessment.order.util;
+
+import com.assessment.order.dto.*;
+import com.assessment.order.model.*;
+
+/**
+ * @author Krishna Chaitanya
+ */
+public final class OrderUtil {
+
+    private OrderUtil() throws IllegalAccessException {
+        throw new IllegalAccessException("can't be instantiated");
+    }
+
+    public static OrderDetails convertOrderRequestToOrderDetails(Order order, OrderRequest orderRequest) {
+        return new OrderDetails(
+                order.getId(),
+                order.getCustomer().getId(),
+                orderRequest.restaurantId(),
+                order.getDeliveryAddress(),
+                order.getBillingAmount(),
+                orderRequest.items()
+        );
+    }
+}
