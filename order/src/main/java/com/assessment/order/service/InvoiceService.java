947d05662b3b418cfaaa576d1e9585912a4e52dc
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-11-26 20:16:00 -0700

    RestTemplate is replaced with RestClient

--- order/src/main/java/com/assessment/order/service/InvoiceService.java
+++ order/src/main/java/com/assessment/order/service/InvoiceService.java
@@ -4,6 +4,7 @@ import com.assessment.order.dto.*;
 import lombok.*;
 import lombok.extern.slf4j.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.client.*;
 
@@ -18,12 +19,26 @@ public class InvoiceService {
     @Value("${invoice.base.uri}")
     private String invoiceBaseUri;
 
-    private final RestTemplate restTemplate;
+//    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     public boolean sendInvoice(final OrderDetails orderDetails) {
+        /**
         var invoiceResponseResponseEntity = restTemplate.postForEntity(invoiceBaseUri, orderDetails, InvoiceResponse.class);
         log.info("invoice api status code : {}, response : {}", invoiceResponseResponseEntity.getStatusCode(), invoiceResponseResponseEntity.getBody());
         return true;
+         **/
+        var invoiceResponseResponseEntity =
+                restClient
+                        .post()
+                        .uri(invoiceBaseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(orderDetails)
+                        .retrieve()
+                        .toEntity(InvoiceResponse.class);
+        log.info("invoice api status code : {}, response : {}", invoiceResponseResponseEntity.getStatusCode(), invoiceResponseResponseEntity.getBody());
+        return true;
+
     }
 
 }
